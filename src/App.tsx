cab1bf50f0b62c92185134298ae5fcfa0a569923
AugustoFicara <augustoficara04@gmail.com>
2024-03-29 18:22:19 -0300

    arreglo de header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import Header from "./components/Header/Header";
+import Header from "./components/Header";
 import Footer from "./components/Footer/Footer";
 import AppRoutes from "./routes/AppRoutes";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Container } from "react-bootstrap";
+import '../src/App.css'
 
 const App = () => {
   return (
     <Router>
-      <header><Header  /></header>
+      <Header  />
       <Container >
         <AppRoutes />
       </Container>
