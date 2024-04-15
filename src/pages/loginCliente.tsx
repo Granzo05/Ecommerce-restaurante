127d7e88897cecaf4227c3ed72475190c2bdf181
AugustoFicara <augustoficara04@gmail.com>
2024-04-15 13:37:30 -0300

    login mejoradisimo

--- src/pages/loginCliente.tsx
+++ src/pages/loginCliente.tsx
@@ -1,6 +1,9 @@
 import { useState } from 'react'
 import '../styles/login.css';
 import { ClienteService } from '../services/ClienteService'
+import LockIcon from '@mui/icons-material/Lock';
+import LockOpenIcon from '@mui/icons-material/LockOpen';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
 
 const LoginCliente = () => {
@@ -19,13 +22,39 @@ const LoginCliente = () => {
         ClienteService.createUser(nombre, apellido, email, contraseña, telefono, domicilio);
     };
 
+    const [tipoInput, setTipoInput] = useState('password');
+    const [mostrarIniciarSesion, setMostrarIniciarSesion] = useState(true);
+    const [mostrarReestablecerContraseña, setMostrarReestablecerContraseña] = useState(false);
+    const [mostrarCrearCuenta, setMostrarCrearCuenta] = useState(false);
+
+    const toggleTipoInput = () => {
+        setTipoInput(tipoInput === 'password' ? 'text' : 'password');
+    };
+
+    const mostrarSeccion = (seccion: string) => {
+        if (seccion === 'iniciarSesion') {
+            setMostrarIniciarSesion(true);
+            setMostrarReestablecerContraseña(false);
+            setMostrarCrearCuenta(false);
+        } else if (seccion === 'reestablecerContraseña') {
+            setMostrarIniciarSesion(false);
+            setMostrarReestablecerContraseña(true);
+            setMostrarCrearCuenta(false);
+        } else if (seccion === 'crearCuenta') {
+            setMostrarIniciarSesion(false);
+            setMostrarReestablecerContraseña(false);
+            setMostrarCrearCuenta(true);
+        }
+    };
+
     return (
         <body>
             {/*INICIAR SESION*/}
-            <section className="form-main">
+            <section className="form-main" style={{ display: mostrarIniciarSesion ? '' : 'none' }}>
                 <div className="form-content">
                     <div className="box">
-                        <h3>¡BIENVENIDO!</h3>
+                        <h3>- BIENVENIDO -</h3>
+                        <p id='subtitle'>¡Si ya tienes una cuenta, inicia sesión con tus datos!</p>
                         <form action="">
                             <div className="input-box">
                                 <label>
@@ -34,25 +63,46 @@ const LoginCliente = () => {
                             </div>
                             <div className="input-box">
                                 <label>
-                                    <input required type="password" className='input-control' placeholder="Contraseña" onChange={(e) => { setContraseña(e.target.value) }} />
+                                    <input required type={tipoInput} className='input-control' placeholder="Contraseña" onChange={(e) => { setContraseña(e.target.value) }} />
+
                                 </label>
+                                <i id='icon-lock' onClick={toggleTipoInput}>{tipoInput === 'password' ? <LockIcon /> : <LockOpenIcon />}</i>
                                 <div className="input-link">
-                                    <a href="#" className='gradient-text'>¿Has olvidado tu contraseña?</a>
+                                    <p>¿Has olvidado tu contraseña?&nbsp;<a href="#" className='gradient-text' onClick={() => mostrarSeccion('reestablecerContraseña')}>Click aquí</a></p>
                                 </div>
                             </div>
-                            <input type="button" className='btn' value="Iniciar sesión" onClick={handleIniciarSesionUsuario} />
+                            <input type="button" className='btn' value="INICIAR SESIÓN" onClick={handleIniciarSesionUsuario} />
                         </form>
-                        <p>¿No tienes una cuenta? <a href="#" className='gradient-text'>Crear cuenta</a></p>
+                        <p id='create-account'>¿No tienes una cuenta?&nbsp;<a href="#" className='gradient-text' onClick={() => mostrarSeccion('crearCuenta')}>Crear cuenta</a></p>
+                    </div>
+                </div>
+            </section>
+
+            {/*CONTRASEÑA OLVIDADA*/}
+            <section className="form-main" style={{ display: mostrarReestablecerContraseña ? '' : 'none' }}>
+                <div className="form-content">
+                    <div className="box">
+                        <h2 id='back-icon' onClick={() => mostrarSeccion('iniciarSesion')}><KeyboardBackspaceIcon></KeyboardBackspaceIcon></h2>
+                        <h3>- REESTABLECER CONTRASEÑA -</h3>
+                        <p id='subtitle'>¡Necesitamos que coloques un correo electrónico para ayudarte a reestablecer tu contraseña!</p>
+                        <form action="">
+                            <div className="input-box">
+                                <label>
+                                    <input required type="email" className='input-control' placeholder="Correo electrónico" onChange={(e) => { setEmail(e.target.value) }} />
+                                </label>
+                            </div>
+                            <input type="button" className='btn' value="ENVIAR CORREO DE RECUPERACIÓN" onClick={handleIniciarSesionUsuario} style={{ marginTop: '0.5px' }} />
+                        </form>
                     </div>
                 </div>
             </section>
 
             {/*CREAR CUENTA*/}
-            <section className="form-main" style={{display: 'none'}}>
+            <section className="form-main" style={{ display: mostrarCrearCuenta ? '' : 'none' }}>
                 <div className="form-content">
                     <div className="box">
-                        <h3>CREAR UNA CUENTA</h3>
-                        <p>o usa tu e-mail para registrarte</p>
+                        <h3>- CREAR UNA CUENTA -</h3>
+                        <p id='subtitle'>o registrate con: <img id='icon-gmail' src="https://img.icons8.com/color/48/gmail-new.png" alt="gmail-new"/></p>
                         <form action="">
                             <div className="input-box">
                                 <label>
@@ -76,17 +126,19 @@ const LoginCliente = () => {
                             </div>
                             <div className="input-box">
                                 <label>
-                                    <input required type="password" className='input-control' placeholder="Contraseña" onChange={(e) => { setContraseña(e.target.value) }} />
+                                    <input required type={tipoInput} className='input-control' placeholder="Contraseña" onChange={(e) => { setContraseña(e.target.value) }} />
                                 </label>
+                                <i id='icon-lock' onClick={toggleTipoInput}>{tipoInput === 'password' ? <LockIcon /> : <LockOpenIcon />}</i>
+
                             </div>
                             <div className="input-box">
                                 <label>
                                     <input required type="text" className='input-control' placeholder="Domicilio" onChange={(e) => { setDomicilio(e.target.value) }} />
                                 </label>
                             </div>
-                            <input type="button" className='btn' value="Registrarse" onClick={handleCargarUsuario} />
+                            <input style={{marginTop: '1px'}} type="button" className='btn' value="REGISTRARSE" onClick={handleCargarUsuario} />
                         </form>
-                        <p>¿Ya tienes una cuenta? <a href="#" className='gradient-text'>Iniciar sesión</a></p>
+                        <p id='subtitle'>¿Ya tienes una cuenta?&nbsp;<a href="#" className='gradient-text' onClick={() => mostrarSeccion('iniciarSesion')}>Iniciar sesión</a></p>
                     </div>
                 </div>
             </section>
