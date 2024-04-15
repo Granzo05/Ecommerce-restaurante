cfa558fa7dd1ed1e0300615a7505f61f4f28e506
AugustoFicara <augustoficara04@gmail.com>
2024-04-14 23:34:02 -0300

    footer final

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,76 +1,85 @@
 import { MDBFooter, MDBContainer, MDBRow, MDBCol, MDBIcon } from 'mdb-react-ui-kit';
+import '../styles/footer.css'
 
 const Footer = () => {
     return (
-            <MDBFooter className='text-center text-lg-start text-muted'>
-                <section className=''>
-                    <MDBContainer className='text-center text-md-start mt-5' style={{color: 'white'}}>
-                        <MDBRow className='mt-3'>
-                            <MDBCol md="3" lg="4" xl="3" className='mx-auto mb-4' >
-                                <img src="./src/assets/img/HatchfulExport-All/logo_transparent_header.png" alt="Logo" style={{width: '248px', display: 'flex', marginTop: '3px' }}/>
-                                <br />
-                                <p>
-                                    ¡Tu comida a la velocidad del rayo!
-                                </p>
-                            </MDBCol>
-
-                            <MDBCol md="2" lg="2" xl="2" className='mx-auto mb-4'>
-                                <h6 className='text-uppercase fw-bold mb-4'>- Legal -</h6>
-                                <p>
-                                    <a href='#!' className='text-reset'>
-                                        Términos y condiciones
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href='#!' className='text-reset'>
-                                        Privacidad
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href='#!' className='text-reset'>
-                                        Defensa del consumidor
-                                    </a>
-                                </p>
-                            </MDBCol>
-
-                            <MDBCol md="3" lg="2" xl="2" className='mx-auto mb-4'>
-                                <h6 className='text-uppercase fw-bold mb-4'>- Nosotros -</h6>
-                                <p>
-                                    <a href='#!' className='text-reset'>
-                                        ¿Quiénes somos?
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href='#!' className='text-reset'>
-                                        Mejores comidas
-                                    </a>
-                                </p>
-                                <p>
-                                    <a href='#!' className='text-reset'>
-                                        Promociones
-                                    </a>
-                                </p>
-
-                            </MDBCol>
-
-                            <MDBCol md="4" lg="3" xl="3" className='mx-auto mb-md-0 mb-4'>
-                                <h6 className='text-uppercase fw-bold mb-4'>- Contáctanos -</h6>
-                                <p>
-                                    <MDBIcon icon="home" className="me-2" />
-                                    Mendoza, Argentina
-                                </p>
-                                <p>
-                                    <MDBIcon icon="envelope" className="me-3" />
-                                    contactodelbuensabor@gmail.com
-                                </p>
-                                <p>
-                                    <MDBIcon icon="phone" className="me-3" /> + 261 255 3696
-                                </p>
-                            </MDBCol>
-                        </MDBRow>
-                    </MDBContainer>
-                </section>
-            </MDBFooter>
+        <footer className="footer">
+        <div className="footer-container">
+          <div className="footer-newsletter">
+            <h1 className="footer-newsletter__header">
+              Want us to email you with the latest blockbuster news?
+            </h1>
+            <div className="footer-newsletter__form">
+              <input type="email" name="email" placeholder="example@xyz.com"/>
+              <button type="submit" className="submit-btn">Subscribe</button>
+            </div>
+          </div>
+          <div className="footer-body">
+            <div className="footer-body__content">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nobis
+                esse illo culpa aperiam minima odit? Nihil quod illo pariatur
+                libero.
+              </p>
+            </div>
+            <nav className="footer-body__nav">
+              <ul className="footer-body__nav-list">
+                <li className="footer-body__nav-item">
+                  Services
+                  <ul className="footer-body__nav-sublist">
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Marketing</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Design</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">App Development</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Web development</a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="footer-body__nav-item">
+                  About
+                  <ul className="footer-body__nav-sublist">
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">About</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Careers</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">History</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Our Team</a>
+                    </li>
+                  </ul>
+                </li>
+                <li className="footer-body__nav-item">
+                  Support
+                  <ul className="footer-body__nav-sublist">
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">FAQs</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Contact</a>
+                    </li>
+                    <li className="footer-body__nav-subitem">
+                      <a href="" className="footer-body__nav-link">Live chat</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </nav>
+          </div>
+          <div className="footer-attribute">
+              <p>&copy; Company 2024. All right reserved.</p>
+          </div>
+        </div>
+      </footer>
     )
 }
 
