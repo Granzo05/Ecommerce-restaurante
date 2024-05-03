5d8bb8377b547f1a3c3d4ebb02564e1250052979
Granzo <facu.granzotto5@gmail.com>
2024-05-03 20:16:06 -0300

    StockEntrante carga

--- server/api/src/main/java/main/repositories/DetalleStockRepository.java
+++ server/api/src/main/java/main/repositories/DetalleStockRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FechaStockRepository extends JpaRepository<DetalleStock, Long> {
+public interface DetalleStockRepository extends JpaRepository<DetalleStock, Long> {
 
 }
