d85604b2e609b34edaa862cf3f622d0990db8b74
Granzo <facu.granzotto5@gmail.com>
2024-05-03 20:23:39 -0300

    Stock entrante mejorado

---
+++ src/components/StockEntrante/StockEntrante.tsx
@@ -0,0 +1,107 @@
+import { useEffect, useState } from "react";
+import ModalCrud from "../ModalCrud";
+import EliminarStock from "./EliminarStockEntrante";
+import EditarStock from "./EditarStockEntrante";
+import '../../styles/stock.css';
+import { StockEntranteService } from "../../services/StockEntranteService";
+import { StockEntrante } from "../../types/Stock/StockEntrante";
+import AgregarStockEntrante from "./AgregarStockEntrante";
+
+const Stocks = () => {
+    const [stockEntrante, setStockEntrante] = useState<StockEntrante[]>([]);
+    const [mostrarStocks, setMostrarStocks] = useState(true);
+
+    const [showAgregarStockModal, setShowAgregarStockModal] = useState(false);
+    const [showEditarStockModal, setShowEditarStockModal] = useState(false);
+    const [showEliminarStockModal, setShowEliminarStockModal] = useState(false);
+
+    const [selectedStock, setSelectedStock] = useState<StockEntrante | null>(null);
+
+    useEffect(() => {
+
+        StockEntranteService.getStock()
+            .then(data => {
+                setStockEntrante(data);
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    }, []);
+
+    const handleAgregarStock = () => {
+        setShowAgregarStockModal(true);
+        setMostrarStocks(false);
+    };
+
+    const handleEditarStock = (stock: StockEntrante) => {
+        setSelectedStock(stock);
+        setShowEditarStockModal(true);
+        setMostrarStocks(false);
+
+    };
+
+    const handleEliminarStock = (stock: StockEntrante) => {
+        setSelectedStock(stock);
+        setShowEliminarStockModal(true);
+        setMostrarStocks(false);
+    };
+
+    const handleModalClose = () => {
+        setShowAgregarStockModal(false);
+        setShowEditarStockModal(false);
+        setShowEliminarStockModal(false);
+
+        setMostrarStocks(true);
+    };
+
+    return (
+        <div className="opciones-pantallas">
+
+            <h1>Stocks</h1>
+            <button onClick={() => handleAgregarStock()}> + Agregar stock entrante</button>
+
+            <ModalCrud isOpen={showAgregarStockModal} onClose={handleModalClose}>
+                <AgregarStockEntrante />
+            </ModalCrud>
+
+            {mostrarStocks && (
+                <div id="stocks">
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Sucursal</th>
+                                <th>Fecha</th>
+                                <th>Costo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {stockEntrante.map(stock => (
+                                <tr key={stock.id}>
+                                    <td>{stock?.sucursal?.id}</td>
+                                    <td>{stock.fechaLlegada?.toDateString()}</td>
+                                    <td>{stock.costo}</td>
+                                    <td>
+                                        <button onClick={() => handleEliminarStock(stock)}>ELIMINAR</button>
+                                        <button onClick={() => handleEditarStock(stock)}>EDITAR</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+
+                    <ModalCrud isOpen={showEliminarStockModal} onClose={handleModalClose}>
+                        {selectedStock && <EliminarStock stockEntrante={selectedStock} />}
+                    </ModalCrud>
+
+                    <ModalCrud isOpen={showEditarStockModal} onClose={handleModalClose}>
+                        {selectedStock && <EditarStock stockEntrante={selectedStock} />}
+                    </ModalCrud>
+
+                </div>
+            )}
+
+        </div>
+    )
+}
+
+export default Stocks
--- src/types/Stock/StockEntrante.ts
+++ src/types/Stock/StockEntrante.ts
@@ -1,9 +1,12 @@
+import { Sucursal } from "../Restaurante/Sucursal";
 import { DetalleStock } from "./DetalleStock";
 
 export class StockEntrante {
     id: number = 0;
     fechaLlegada: Date | null = null;
+    costo: number = 0;
     detallesStock: DetalleStock[] = [];
+    sucursal: Sucursal | null = null;
 
     constructor() {
 
