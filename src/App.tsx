4d75794983a3b2d484f707e0db8b1c4b2fcfaa6a
Granzo <facu.granzotto5@gmail.com>
2024-05-05 21:33:20 -0300

    sacar header y footer

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,36 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import AppRoutes from './routes/AppRoutes';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [mostrarHeader, setMostrarHeader] = useState(true)
+
+  useEffect(() => {
+    const ruta = window.location.href.split('/').pop(); 
+    console.log(ruta)
+    if (ruta === 'login-cliente') {
+      setMostrarHeader(false);
+    }
+  }, []);
+
+
   return (
+
     <Router>
-      <Header />
-      <AppRoutes />
-      <Footer />
+      {mostrarHeader ? (
+        <div>
+          <Header />
+          <AppRoutes />
+          <Footer />
+        </div>
+      ) : (
+        <div>
+          <AppRoutes />
+        </div>
+      )}
     </Router>
+
   );
 }
 
