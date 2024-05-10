f20b0221c0e32bb0c453f122089c077a4651c732
AugustoFicara <augustoficara04@gmail.com>
2024-05-09 22:33:21 -0300

    headerloginrestau hecho

--- src/components/headerLogin.tsx
+++ src/components/headerLogin.tsx
@@ -2,12 +2,13 @@ import Logo from '../assets/img/HatchfulExport-All/logo_transparent_header.png'
 import '../styles/headerLogin.css'
 
 const HeaderLogin = () => {
-    
+
 
     return (
-        <header id='header-login'>
-            <img id='logo-header-login' src={Logo}/>
-        </header>
+            <header id='header-login'>
+                <img id='logo-header-login' src={Logo} />
+            </header>
+
     )
 
 }
