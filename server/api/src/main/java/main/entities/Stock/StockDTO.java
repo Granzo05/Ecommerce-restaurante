03b1198a22d058c88bf2f43b982960af84215d0e
Granzo <facu.granzotto5@gmail.com>
2024-05-14 11:12:27 -0300

    Stock dto

---
+++ server/api/src/main/java/main/entities/Stock/StockDTO.java
@@ -0,0 +1,45 @@
+package main.entities.Stock;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import main.entities.Ingredientes.EnumMedida;
+import main.entities.Restaurante.Sucursal;
+import net.minidev.json.annotate.JsonIgnore;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public abstract class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "precio_compra")
+    private double precioCompra;
+    @Column(name = "cantidad_actual")
+    private int cantidadActual;
+    @Column(name = "cantidad_minima")
+    private int cantidadMinima;
+    @Column(name = "cantidad_maxima")
+    private int cantidadMaxima;
+    @Column(name = "medida")
+    private EnumMedida medida;
+    @JsonIgnore
+    @Column(name = "borrado")
+    private String borrado = "NO";
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_sucursal")
+    private Sucursal sucursal;
+
+    public Stock(double precioCompra, int cantidadActual, int cantidadMinima, int cantidadMaxima, EnumMedida medida) {
+        this.precioCompra = precioCompra;
+        this.cantidadActual = cantidadActual;
+        this.cantidadMinima = cantidadMinima;
+        this.cantidadMaxima = cantidadMaxima;
+        this.medida = medida;
+    }
+}
