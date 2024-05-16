30f793e3237519147c1a6c84e571a7461cb4dddf
AugustoFicara <augustoficara04@gmail.com>
2024-05-16 17:19:26 -0300

    footer, headers fletados

--- src/App.tsx
+++ src/App.tsx
@@ -2,48 +2,43 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import AppRoutes from './routes/AppRoutes';
-import HeaderLogin from './components/headerLogin'
+import HeaderLogin from './components/headerLogin';
 import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [mostrarHeader, setMostrarHeader] = useState(true)
+  const [mostrarFooter, setMostrarFooter] = useState(true);
+  const [mostrarHeader, setMostrarHeader] = useState(true);
+  const [mostrarHeaderLogin, setMostrarHeaderLogin] = useState(true);
 
   useEffect(() => {
     const ruta = window.location.href.split('/').pop(); 
-    if (ruta === 'login-cliente') {
+    
+    if (ruta === 'opciones' || ruta === 'login-negocio') {
+      setMostrarFooter(false);
+    } else {
+      setMostrarFooter(true);
+    }
+
+    if (ruta === 'opciones' || ruta === 'login-negocio') {
       setMostrarHeader(false);
-    }else if (ruta === 'login-cliente#'){
-      setMostrarHeader(false);
-    }else if (ruta === 'login-negocio'){
-      setMostrarHeader(false);
-    }else if (ruta === 'login-negocio#'){
-      setMostrarHeader(false);
-    }else if (ruta === 'cambio-contra'){
-      setMostrarHeader(false);
+    } else {
+      setMostrarHeader(true);
+    }
+
+    if (ruta === 'opciones') {
+      setMostrarHeaderLogin(false);
+    } else {
+      setMostrarHeaderLogin(true);
     }
   }, []);
 
-
   return (
-
     <Router>
-      {mostrarHeader ? (
-        <div>
-          <Header />
-          <AppRoutes />
-          
-        </div>
-      ) : (
-        <div>
-          <HeaderLogin/>
-          <AppRoutes />
-          
-        </div>
-      )}
-      <Footer />
+      {mostrarHeader ? <Header /> : mostrarHeaderLogin ? <HeaderLogin /> : null}
+      <AppRoutes />
+      {mostrarFooter && <Footer />}
     </Router>
-
   );
 }
 
