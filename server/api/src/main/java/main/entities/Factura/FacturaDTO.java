c974c06c7251bf49dc08b3ef2c2a15305ce9a772
Granzo <facu.granzotto5@gmail.com>
2024-05-17 14:46:11 -0300

    Dto

---
+++ server/api/src/main/java/main/entities/Factura/FacturaDTO.java
@@ -0,0 +1,36 @@
+package main.entities.Factura;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import main.entities.Pedidos.Pedido;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "facturas", schema = "buen_sabor")
+public class Factura {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "tipo_factura")
+    private EnumTipoFactura tipoFactura;
+    @Column(name = "metodo_pago")
+    private EnumMetodoPago metodoPago;
+    @Column(name = "fecha_creacion", updatable = false, nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    public Date fechaFacturacion;
+    @Column(name = "total")
+    private double total;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pedido")
+    private Pedido pedido;
+}
---
+++ server/api/src/main/java/main/entities/Pedidos/DetallesPedidoDTO.java
@@ -0,0 +1,33 @@
+package main.entities.Pedidos;
+
+import jakarta.persistence.*;
+import lombok.*;
+import main.entities.Productos.ArticuloMenu;
+import main.entities.Productos.ArticuloVenta;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "detalles_pedido", schema = "buen_sabor")
+public class DetallesPedido {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "cantidad")
+    private int cantidad;
+    @Column(name = "subtotal")
+    private double subTotal;
+    @OneToOne
+    @JoinColumn(name = "id_menu")
+    private ArticuloMenu articuloMenu;
+    @OneToOne
+    @JoinColumn(name = "id_articulo")
+    private ArticuloVenta articuloVenta;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "id_pedido")
+    private Pedido pedido;
+
+}
