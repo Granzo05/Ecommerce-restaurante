85d0c9fb795ac526b1bad06f0f35ea3405ea5dee
Granzo <facu.granzotto5@gmail.com>
2024-05-22 19:45:05 -0300

    rutas

--- server/api/src/main/java/main/Main.java
+++ server/api/src/main/java/main/Main.java
@@ -58,8 +58,7 @@ public class Main {
 
     @Autowired(required = true)
     private EmpresaRepository empresaRepository;
-    private final String RUTACSV = "E://Facultad//2do Año//4to Semestre//Proyecto Final//buen-sabor-app-typescript-react//server//api//src//main//resources//localidades.csv";
-    private final String RUTACSV2 = "D://Buen-sabor//buen-sabor-app-typescript-react//server//api//src//main//resources//localidades.csv";
+    private final String RUTACSV = "C://Buen-sabor//buen-sabor-app-typescript-react//server//api//src//main//resources//localidades.csv";
     private final String SEPARACIONCSV = ";";
 
     @Bean
@@ -83,18 +82,7 @@ public class Main {
                     paisRepository.save(pais);
 
                 } catch (IOException e) {
-                    try (BufferedReader br = new BufferedReader(new FileReader(RUTACSV2))) {
-                        Pais pais = crearPais("Argentina");
-                        String line;
-
-                        while ((line = br.readLine()) != null) {
-                            String[] data = line.split(SEPARACIONCSV);
-                            cargarDatos(data[0], data[1], data[2], pais);
-                        }
-
-                        paisRepository.save(pais);
-
-                    }
+                    System.out.println(e);
                 }
             }
 
