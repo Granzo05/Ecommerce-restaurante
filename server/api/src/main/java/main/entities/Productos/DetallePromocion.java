fb6edb7eea4bd4e994c1ee639b63d839b286106f
Granzo <facu.granzotto5@gmail.com>
2024-05-29 17:26:33 -0300

    f

---
+++ server/api/src/main/java/main/entities/Productos/DetallePromocion.java
@@ -0,0 +1,45 @@
+package main.entities.Stock;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import main.entities.Ingredientes.Ingrediente;
+import main.entities.Ingredientes.Medida;
+import main.entities.Productos.ArticuloVenta;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@ToString
+@Table(name = "detalle_stock", schema = "buen_sabor")
+public class DetalleStock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "cantidad")
+    private int cantidad;
+    @JsonIgnoreProperties(value = {"sucursales", "detalleStock"})
+    @ManyToOne
+    @JoinColumn(name = "id_medida")
+    private Medida medida;
+    @Column(name = "costo_unitario")
+    private double costoUnitario;
+    @Column(name = "subtotal")
+    private double subTotal;
+    @ManyToOne
+    @JsonIgnoreProperties(value = {"stock"})
+    @JoinColumn(name = "id_ingrediente")
+    private Ingrediente ingrediente;
+    @ManyToOne
+    @JsonIgnoreProperties(value = {"stock"})
+    @JoinColumn(name = "id_articulo")
+    private ArticuloVenta articuloVenta;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnoreProperties(value = {"sucursal"})
+    @JoinColumn(name = "id_stock_entrante")
+    private StockEntrante stockEntrante;
+
+}
