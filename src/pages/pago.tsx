0e2ff265ab7f80d7b86517dc11330b640a251c2b
AugustoFicara <augustoficara04@gmail.com>
2024-05-29 23:15:21 -0300

    pago listo

--- src/pages/pago.tsx
+++ src/pages/pago.tsx
@@ -100,15 +100,16 @@ const Pago = () => {
         <div className="container-pago">
             <Toaster />
             <div className="div-pago">
+                
+                <h3>&mdash; Detalle del pedido y pago &mdash;</h3>
                 <div id="detalle-producto"></div>
-                <label style={{fontWeight: 'bold', color: '#2C2C2C'}}>Tipo de entrega:</label>
+                <label style={{fontWeight: 'bold', color: '#2C2C2C', fontSize: '18px'}}>Tipo de entrega:</label>
                 <select className="tipo-envio" name="tipoEnvio" id="tipoEnvio" onChange={e => setTipoEnvio(e.target.value)}>
                     <option value="DELIVERY">Delivery</option>
                     <option value="RETIRO">Retiro en tienda</option>
                 </select>
                 <hr />
-                <h2>Detalles del pedido</h2>
-
+                <label style={{fontWeight: 'bold', color: '#2C2C2C', fontSize: '18px'}}>Domicilio a enviar:</label>
                 {envio === 'DELIVERY' && (
                     domicilios && domicilios.length > 0 && (
                         <select id="domicilioSeleccionado" name="domicilioSeleccionado">
@@ -120,6 +121,9 @@ const Pago = () => {
                         </select>
                     )
                 )}
+                <hr />
+                <label style={{fontWeight: 'bold', color: '#2C2C2C', fontSize: '18px'}}>Detalles del pedido:</label>
+                
 
                 <div className="productos">
                     {carrito && carrito.articuloMenu.map((producto, index) => (
@@ -127,9 +131,12 @@ const Pago = () => {
                             {producto.imagenesDTO[0] && (
                                 <img src={producto.imagenesDTO[0].ruta} alt={producto.nombre} />
                             )}
-                            <p>{producto.nombre}</p>
-                            <p>Cantidad: {carrito.articuloMenu[index].cantidad}</p>
-                            <p>Subtotal: ${carrito.articuloMenu[index].cantidad * carrito.articuloMenu[index].precioVenta}</p>
+                            <p className="name-product">{producto.nombre}</p>
+                            <div className="cant-sub">
+                            <p className="cant-product"><strong>Cantidad:</strong> {carrito.articuloMenu[index].cantidad}</p>
+                            <p className="subtotal-product"><strong>Subtotal:</strong> ${carrito.articuloMenu[index].cantidad * carrito.articuloMenu[index].precioVenta}</p>
+                            
+                            </div>
                         </div>
                     ))}
                     {carrito && carrito.articuloVenta.map((producto, index) => (
@@ -137,18 +144,22 @@ const Pago = () => {
                             {producto.imagenesDTO[0] && (
                                 <img src={producto.imagenesDTO[0].ruta} alt={producto.nombre} />
                             )}
-                            <p>{producto.nombre}</p>
-                            <p >Cantidad: {carrito.articuloVenta[index].cantidad}</p>
-                            <p>Subtotal: ${carrito.articuloVenta[index].cantidad * carrito.articuloVenta[index].precioVenta}</p>
-                        
+                            <p className="name-product">{producto.nombre}</p>
+                            <div className="cant-sub">
+                            <p className="cant-product"><strong>Cantidad:</strong> {carrito.articuloVenta[index].cantidad}</p>
+                            <p className="subtotal-product"><strong>Subtotal:</strong> ${carrito.articuloVenta[index].cantidad * carrito.articuloVenta[index].precioVenta}</p>
+                            
+                            </div>
+                            
                         </div>
                     ))}
                     <hr />
                 </div>
 
+
                 {envio === 'DELIVERY' ? (
                     <div className="total">
-                        <h2>Total: ${carrito?.totalPrecio}</h2>
+                        <h2><strong>Total:</strong> ${carrito?.totalPrecio}</h2>
 
                         <button
                             type="submit"
@@ -168,9 +179,9 @@ const Pago = () => {
                 ) : (
                     <div className="total">
                         {carrito?.totalPrecio && (
-                            <h2>Total: ${carrito.totalPrecio * 0.9}</h2>
+                            <h2><strong>Total:</strong> ${carrito.totalPrecio * 0.9}</h2>
                         )}
-                        <p>*Retiro en tienda con 10% de descuento</p>
+                        <p style={{color: '#007bff'}}>*Retiro en tienda con 10% de descuento</p>
                         <button
                             type="submit"
                             className="checkout-btn"
