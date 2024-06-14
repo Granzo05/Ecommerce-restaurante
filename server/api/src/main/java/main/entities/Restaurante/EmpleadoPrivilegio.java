635f2e34033832242628ed6db212bcd56500e6c9
Granzo <facu.granzotto5@gmail.com>
2024-06-14 11:15:40 -0300

    creacion de privilegios

---
+++ server/api/src/main/java/main/entities/Restaurante/EmpleadoPrivilegio.java
@@ -0,0 +1,2 @@
+package main.entities.Restaurante;public class EmpleadoPrivilegio {
+}
---
+++ server/api/src/main/java/main/entities/Restaurante/Privilegios.java
@@ -0,0 +1,60 @@
+package main.entities.Restaurante;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.*;
+import main.entities.Domicilio.Domicilio;
+import net.minidev.json.annotate.JsonIgnore;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name = "empleados", schema = "buen_sabor")
+public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "email")
+    private String email;
+    @Column(name = "contraseña")
+    private String contraseña;
+    @Column(name = "cuil")
+    private String cuil;
+    @Column(name = "telefono")
+    private Long telefono;
+
+    @JsonIgnoreProperties(value = {"domicilios"}, allowSetters = true)
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "empleado", cascade = CascadeType.ALL)
+    private Set<Domicilio> domicilios = new HashSet<>();
+
+    @JsonIgnoreProperties(value = {"empleado"}, allowSetters = true)
+    @OneToMany(mappedBy = "empleado", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.EAGER)
+    private Set<FechaContratacionEmpleado> fechaContratacion = new HashSet<>();
+
+    @Column(name = "fecha_nacimiento", nullable = false)
+    private LocalDate fechaNacimiento;
+
+    @Column(name = "borrado")
+    private String borrado = "NO";
+
+    @OneToMany(mappedBy = "empleado")
+    private Set<Privilegios> privilegios;
+
+    @JsonIgnoreProperties(value = {"empleados", "empresa", "stocksIngredientes", "stocksArticulo", "promociones", "localidadesDisponiblesDelivery", "articulosMenu", "articulosVenta", "medidas", "categorias", "imagenes", "ingredientes", "stocksEntranteSucursal"}, allowSetters = true)
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "empleados_sucursales",
+            joinColumns = @JoinColumn(name = "id_empleado"),
+            inverseJoinColumns = @JoinColumn(name = "id_sucursal")
+    )
+    private Set<Sucursal> sucursales = new HashSet<>();
+}
